1fdfaee3e342fa78bf576cb8c669fb05414eee35
Aditya Anil Jadhav <161210841+aditya78230@users.noreply.github.com>
2024-11-25 00:55:35 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://mern-task-backend-6w3i.onrender.com'; 
+const API_URL = 'http://localhost:5000'; 
 
 export const loginUser = async (userData) => {
   try {
